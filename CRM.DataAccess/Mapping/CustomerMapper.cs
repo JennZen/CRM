0fbc2719a3bd355080cacd99f01badb0adf1e1bf
JennZen <zlodard@gmail.com>
2026-09-03 11:57:54 +0300

    Add mappers

---
+++ CRM.DataAccess/Mapping/CustomerMapper.cs
@@ -0,0 +1,22 @@
+﻿using CRM.DataAccess.Models;
+using CRM.Domain.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.DataAccess.Mapping
+{
+    [Mapper]
+    public partial class CustomerMapper
+    {
+        [MapperIgnoreTarget(nameof(Customer.Requests))]
+        [MapProperty(nameof(CustomerDb.Oid), nameof(Customer.Id))]
+        public partial Customer ToDomain(CustomerDb c);
+
+        public partial List<Customer> ToDomains(List<CustomerDb> c);
+
+    }
+}
---
+++ CRM.DataAccess/Mapping/RequestMapper.cs
@@ -0,0 +1,23 @@
+﻿using CRM.DataAccess.Models;
+using CRM.Domain.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.DataAccess.Mapping
+{
+    [Mapper]
+    public partial class RequestMapper
+    {
+        [MapProperty(nameof(RequestDb.Oid), nameof(Request.Id))]
+        [MapProperty($"{nameof(RequestDb.Customer)}.{nameof(CustomerDb.Oid)}", nameof(Request.CustomerId))]
+        [MapProperty($"{nameof(RequestDb.Manager)}.{nameof(UserDb.Oid)}", nameof(Request.ManagerId))]
+        public partial Request ToDomain(RequestDb r);
+
+        public partial List<Request> ToDomains(List<RequestDb> r);
+
+    }
+}
