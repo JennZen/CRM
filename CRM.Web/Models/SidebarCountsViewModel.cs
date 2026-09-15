b97aea7ff2c03200292bd57a864894d4044433b1
JennZen <zlodard@gmail.com>
2026-09-15 23:00:30 +0300

    Fix _Layout for Manager

---
+++ CRM.Web/Models/SidebarCountsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRM.Web.Models
+{
+    public class SidebarCountsViewModel
+    {
+        public int CustomersCount { get; set; }
+        
+        public int RequestsCount { get; set; }
+
+        public string CurrentController { get; set; } = "";
+    }
+}
---
+++ CRM.Web/ViewComponents/SidebarCountsViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using CRM.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CRM.Web.ViewComponents
+{
+    public class SidebarCountsViewComponent : ViewComponent
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SidebarCountsViewComponent(IHttpClientFactory httpClientFactory, IHttpContextAccessor accessor)
+        {
+            _httpClient = httpClientFactory.CreateClient("CRM.Api"); ;
+            _httpContextAccessor = accessor;
+        }
+
+        private void AttachToken()
+        {
+            var token = _httpContextAccessor.HttpContext?.Session.GetString("JwtToken");
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            AttachToken();
+
+            var currentController = ViewContext.RouteData.Values["controller"]?.ToString() ?? "";
+            var customersCount = await _httpClient.GetFromJsonAsync<int>("api/customer/count");
+            var requestsCount = await _httpClient.GetFromJsonAsync<int>("api/request/my/count");
+
+            return View(new SidebarCountsViewModel() 
+            { 
+                CustomersCount = customersCount, 
+                RequestsCount = requestsCount,
+                CurrentController = currentController,
+            });
+        }
+    }
+}
